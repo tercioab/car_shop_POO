add730b04bc50114ef73484277712f84af7bade7
walthercio Almeida <suaagenciainicial@gmail.com>
2023-02-13 19:38:29 -0300

    feat: create interface to car

---
+++ src/Interfaces/ICar.ts
@@ -0,0 +1,10 @@
+export default interface ICar {
+  id: string,
+  model: string,
+  year: number,
+  color: string,
+  status?: boolean | false, 
+  buyValue:number, 
+  doorsQty:number, 
+  seatsQty:number, 
+}
