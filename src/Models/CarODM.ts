274b1bfb4134beeda1646228bac786200fbb6f51
walthercio Almeida <suaagenciainicial@gmail.com>
2023-02-13 19:39:01 -0300

    feat: car model

---
+++ src/Models/CarODM.ts
@@ -0,0 +1,25 @@
+import { Schema, Model, model, models } from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+export default class CarODM {
+  private _schema: Schema;
+  private _model: Model<ICar>;
+    
+  constructor() {
+    this._schema = new Schema<ICar>({
+      id: { type: String, required: true },
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: true },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+    this._model = models.Car || model('Car', this._schema);
+  }
+    
+  public async create(car: ICar): Promise<ICar> {
+    return this._model.create({ ...car });
+  }
+}
