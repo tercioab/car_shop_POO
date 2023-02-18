45c6c3283c5fa5a7623c87845d6f05d82a6f0a56
walthercio Almeida <suaagenciainicial@gmail.com>
2023-02-18 17:52:40 -0300

    refactor: tests services

--- tests/unit/Services/Car.test.ts
+++ tests/unit/Services/Car.test.ts
@@ -8,40 +8,36 @@ describe('teste car service', function () {
   afterEach(function () {
     sinon.restore();
   });
-
-  it('testa se é possivel inserir um novo carro', async function () {
+  it('01. testa se é possivel inserir um novo carro', async function () {
     sinon.stub(Model, 'create').resolves(carMocks.carOutPut);
-    const carService = new CarService();
-    const result = await carService.create(carMocks.carInput);
+    const result = await new CarService().create(carMocks.carInput);
     expect(result).to.be.deep.equal(carMocks.carOutPut);
   });
     
-  it('testa se é possivel retornar todos os carros', async function () {
+  it('02. testa se é possivel retornar todos os carros', async function () {
     sinon.stub(Model, 'find').resolves([carMocks.carOutPut]);
     const result = await new CarService().findAll();
     expect(result).to.be.deep.equal([carMocks.carOutPut]);
   });
 
-  it('testa se é possivel retornar o carro pelo id', async function () {
+  it('03. testa se é possivel retornar o carro pelo id', async function () {
     const id = '63ec05e33484f70f15bce3b4'; 
     sinon.stub(Model, 'findById').resolves(carMocks.carOutPut);
     const result = await new CarService().findById(id);
     expect(result).to.be.deep.equal(carMocks.carOutPut);
   });
 
-  it('testa se é possivel atualizar um carro pelo id', async function () {
+  it('04. testa se é possivel atualizar um carro pelo id', async function () {
     const id = '63ec05e33484f70f15bce3b4';
     sinon.stub(Model, 'findByIdAndUpdate').resolves(carMocks.carOutPut);
-    const carService = new CarService();
-    const result = await carService.updateById(id, carMocks.carInput);
+    const result = await new CarService().updateById(id, carMocks.carInput);
     expect(result).to.be.deep.equal(carMocks.carOutPut);
   });
   
-  it('testa se é possivel excluir um carro pelo id', async function () {
+  it('05. testa se é possivel excluir um carro pelo id', async function () {
     const id = '63ec05e33484f70f15bce3b4';
     sinon.stub(Model, 'deleteOne').resolves({ acknowledged: true, deletedCount: 1 });
-    const carService = new CarService();
-    const result = await carService.excludeById(id);
+    const result = await new CarService().excludeById(id);
     expect(result).to.be.deep.equal({ acknowledged: true, deletedCount: 1 });
   });
 });
--- tests/unit/Services/Motorcycle.test.ts
+++ tests/unit/Services/Motorcycle.test.ts
@@ -5,32 +5,36 @@ import MotocicleService from '../../../src/Services/MotocicleService';
 import motorcycleMocks from '../mocks/motorcycle.mocks';
 
 describe('teste motorcycle service', function () {
-  it('testa se é possivel inserir uma nova moto', async function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+  it('01. testa se é possivel inserir uma nova moto', async function () {
     sinon.stub(Model, 'create').resolves(motorcycleMocks.OutPut);
     const motorcycleService = new MotocicleService();
     const result = await motorcycleService.create(motorcycleMocks.Input);
     expect(result).to.be.deep.equal(motorcycleMocks.OutPut);
   });
-  it(' testa se é possivel retornar todas as motos', async function () {
+  
+  it('02. testa se é possivel retornar todas as motos', async function () {
     sinon.stub(Model, 'find').resolves([motorcycleMocks.OutPut]);
     const result = await new MotocicleService().findAll();
     expect(result).to.be.deep.equal([motorcycleMocks.OutPut]);
   });
-  it('testa se é possivel retornar o motos pelo id', async function () {
+  it('03. testa se é possivel retornar o motos pelo id', async function () {
     const id = '6348513f34c397abcad040b2'; 
     sinon.stub(Model, 'findById').resolves(motorcycleMocks.OutPut);
     const result = await new MotocicleService().findById(id);
     expect(result).to.be.deep.equal(motorcycleMocks.OutPut);
   });
 
-  it('testa se é possivel atualizar um motos pelo id', async function () {
+  it('04. testa se é possivel atualizar um motos pelo id', async function () {
     const id = '6348513f34c397abcad040b2';
     sinon.stub(Model, 'findByIdAndUpdate').resolves(motorcycleMocks.OutPut);
     const result = await new MotocicleService().updateById(id, motorcycleMocks.Input);
     expect(result).to.be.deep.equal(motorcycleMocks.OutPut);
   });
   
-  it('testa se é possivel excluir um motos pelo id', async function () {
+  it('05. testa se é possivel excluir um motos pelo id', async function () {
     const id = '6348513f34c397abcad040b2';
     sinon.stub(Model, 'deleteOne').resolves({ acknowledged: true, deletedCount: 1 });
     const result = await new MotocicleService().excludeById(id);
