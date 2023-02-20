46af68fe8bd94d7dc5c7ec871f9f983a107e767e
walthercio Almeida <suaagenciainicial@gmail.com>
2023-02-20 02:28:51 -0300

    refactor: add domains direct on methods

--- src/Services/car.Service.ts
+++ src/Services/car.Service.ts
@@ -5,31 +5,25 @@ import VehicleRepository from '../Domains/repository/Vehicle.repository';
 
 export default class CarService {
   constructor(private Repository: VehicleRepository<ICar>) {}
-  public createVehicleDomain(car: ICar) {
-    if (car) {
-      return new CarDomains(car);
-    } return null;
-  }
-
   public async create(car: ICar) {
     const newCar = await this.Repository.create(car);
-    return this.createVehicleDomain(newCar);
+    return new CarDomains(newCar);
   }
 
   public async findAll() {
     const allCars = await this.Repository.findAll();
     return allCars
-      .map((car) => this.createVehicleDomain(car));
+      .map((car) => new CarDomains(car));
   }
 
   public async findById(id: string) {
     const car = await this.Repository.findById(id);
-    if (car) { return this.createVehicleDomain(car); }
+    if (car) { return new CarDomains(car); }
   }
 
   public async updateById(id: string, vehicle: ICar) {
     const car = await this.Repository.updateById(id, vehicle);
-    if (car) { return this.createVehicleDomain(car); }
+    if (car) { return new CarDomains(car); }
   }
 
   public async excludeById(id: string) {
--- src/Services/motorcycle.Service.ts
+++ src/Services/motorcycle.Service.ts
@@ -3,33 +3,27 @@ import MotorcycleDomains from '../Domains/Motorcycle';
 import VehicleRepository from '../Domains/repository/Vehicle.repository';
 
 export default class MotorcycleService {
-  constructor(private Repository: VehicleRepository<IMotorcycle>) {}
-
-  public createVehicleDomain(motorcycle: IMotorcycle) {
-    if (motorcycle) {
-      return new MotorcycleDomains(motorcycle);
-    } return null;
-  }
-    
+  constructor(private Repository: VehicleRepository<IMotorcycle>) { }
+  
   public async create(car: IMotorcycle) {
     const newMotorcycle = await this.Repository.create(car);
-    return this.createVehicleDomain(newMotorcycle);
+    return new MotorcycleDomains(newMotorcycle);
   }
 
   public async findAll() {
     const allMotorcycle = await this.Repository.findAll();
     return allMotorcycle
-      .map((Motorcycle) => this.createVehicleDomain(Motorcycle));
+      .map((Motorcycle) => new MotorcycleDomains(Motorcycle));
   }
 
   public async findById(id: string) {
     const Motorcycle = await this.Repository.findById(id);
-    if (Motorcycle) { return this.createVehicleDomain(Motorcycle); }
+    if (Motorcycle) { return new MotorcycleDomains(Motorcycle); }
   }
 
   public async updateById(id: string, veicle: IMotorcycle) {
-    const motorcycle = await this.Repository.updateById(id, veicle);
-    if (motorcycle) { return this.createVehicleDomain(motorcycle); }
+    const Motorcycle = await this.Repository.updateById(id, veicle);
+    if (Motorcycle) { return new MotorcycleDomains(Motorcycle); }
   }
 
   public async excludeById(id: string) {
