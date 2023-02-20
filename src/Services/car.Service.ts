e727069630e6717ea0f098664bb5718289a10b79
walthercio Almeida <suaagenciainicial@gmail.com>
2023-02-19 22:39:20 -0300

    refactor: change the name motorcycleODM and carODM in services to Ipersistence

--- src/Services/car.Service.ts
+++ src/Services/car.Service.ts
@@ -3,7 +3,7 @@ import ICar from '../Interfaces/ICar';
 import IVehiclePersistence from '../Domains/repository/IVehicle.persistence';
 
 export default class CarService {
-  constructor(private carODM: IVehiclePersistence) {}
+  constructor(private IPersistence: IVehiclePersistence) {}
   public createVehicleDomain(car: ICar) {
     if (car) {
       return new CarDomains(car);
@@ -11,27 +11,27 @@ export default class CarService {
   }
 
   public async create(car: ICar) {
-    const newCar = await this.carODM.create(car);
+    const newCar = await this.IPersistence.create(car);
     return this.createVehicleDomain(newCar);
   }
 
   public async findAll() {
-    const allCars = await this.carODM.findAll();
+    const allCars = await this.IPersistence.findAll();
     return allCars
       .map((car) => this.createVehicleDomain(car));
   }
 
   public async findById(id: string) {
-    const car = await this.carODM.findById(id);
+    const car = await this.IPersistence.findById(id);
     if (car) { return this.createVehicleDomain(car); }
   }
 
   public async updateById(id: string, vehicle: ICar) {
-    const car = await this.carODM.updateById(id, vehicle);
+    const car = await this.IPersistence.updateById(id, vehicle);
     if (car) { return this.createVehicleDomain(car); }
   }
 
   public async excludeById(id: string) {
-    return this.carODM.excludeById(id);
+    return this.IPersistence.excludeById(id);
   }
 }
--- src/Services/motorcycle.Service.ts
+++ src/Services/motorcycle.Service.ts
@@ -3,7 +3,7 @@ import MotorcycleDomains from '../Domains/entities/Motorcycle';
 import IVehiclePersistence from '../Domains/repository/IVehicle.persistence';
 
 export default class MotorcycleService {
-  constructor(private motorcycleODM: IVehiclePersistence) {}
+  constructor(private IPersistence: IVehiclePersistence) {}
 
   public createVehicleDomain(motorcycle: IMotorcycle) {
     if (motorcycle) {
@@ -12,27 +12,27 @@ export default class MotorcycleService {
   }
     
   public async create(car: IMotorcycle) {
-    const newMotorcycle = await this.motorcycleODM.create(car);
+    const newMotorcycle = await this.IPersistence.create(car);
     return this.createVehicleDomain(newMotorcycle);
   }
 
   public async findAll() {
-    const allMotorcycle = await this.motorcycleODM.findAll();
+    const allMotorcycle = await this.IPersistence.findAll();
     return allMotorcycle
       .map((Motorcycle) => this.createVehicleDomain(Motorcycle));
   }
 
   public async findById(id: string) {
-    const Motorcycle = await this.motorcycleODM.findById(id);
+    const Motorcycle = await this.IPersistence.findById(id);
     if (Motorcycle) { return this.createVehicleDomain(Motorcycle); }
   }
 
   public async updateById(id: string, veicle: IMotorcycle) {
-    const motorcycle = await this.motorcycleODM.updateById(id, veicle);
+    const motorcycle = await this.IPersistence.updateById(id, veicle);
     if (motorcycle) { return this.createVehicleDomain(motorcycle); }
   }
 
   public async excludeById(id: string) {
-    return this.motorcycleODM.excludeById(id);
+    return this.IPersistence.excludeById(id);
   }
 }
