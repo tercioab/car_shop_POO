a2fe0f9d6a1c73a82b406ea3e9ad436370204d4d
walthercio Almeida <suaagenciainicial@gmail.com>
2023-02-20 00:20:35 -0300

    refactor: add vehiclerepository on factories of motorcycles

--- src/Domains/factories/motorcycle.factiores.ts
+++ src/Domains/factories/motorcycle.factiores.ts
@@ -1,9 +1,11 @@
 import MotorcycleController from '../../Controllers/motorcycle.Controller';
 import MotorcycleService from '../../Services/motorcycle.Service';
 import MotorcycleODM from '../../Models/MotorcycleODM';
+import VehicleRepository from '../repository/Vehicle.repository';
 
 const motorcycleODM = new MotorcycleODM();
-const motorcycleService = new MotorcycleService(motorcycleODM);
+const vehicleRepository = new VehicleRepository(motorcycleODM);
+const motorcycleService = new MotorcycleService(vehicleRepository);
 const motorcycleController = new MotorcycleController(motorcycleService);
 
 export default motorcycleController;
